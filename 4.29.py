62a87cf19029cf87cc85bec67ea9502ac35eacd3
linjh08133 <275911105@qq.com>
2021-05-01 17:15:38 +0800

    only greedy algorithm

--- 4.29.py
+++
@@ -1,8 +0,0 @@
-from collections import  Counter
-import numpy as np
-
-x = np.random.randn(333,4)
-mea = x.mean(axis=0)
-var = x.var(axis=0)
-print(mea)
-print(var)
--- 4.30.py
+++
@@ -1,8 +0,0 @@
-import tarfile
-import os
-from six.moves import urllib
-DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
-HOUSING_PATH = "datasets/housing"
-
-print(os.curdir)
-print(os.path.abspath(os.curdir))
